939d9f8a66c890a0e42cc70578eee50861fe480c
myisamkernel <138276597+myisamkernel@users.noreply.github.com>
2025-01-30 09:47:48 +0700

    small change

--- config/db.config.js
+++ config/db.config.js
@@ -1,58 +1,58 @@
 const pg = require('pg')
 
-// const config = {
-//   development: {
-//     username: process.env.USERNAME,
-//     password: process.env.PASSWORD,
-//     database: process.env.DATABASE,
-//     host: process.env.HOST,
-//     dialect: 'postgres',
-//     dialecModule: pg,
-//   },
-//   test: {
-//     username: process.env.USERNAME,
-//     password: process.env.PASSWORD,
-//     database: process.env.DATABASE,
-//     host: process.env.HOST,
-//     dialect: 'postgres',
-//     dialecModule: pg,
-//   },
-//   production: {
-//     username: process.env.USERNAME,
-//     password: process.env.PASSWORD,
-//     database: process.env.DATABASE,
-//     host: process.env.HOST,
-//     dialect: 'postgres',
-//     dialecModule: pg,
-//   },
-// }
-
-// DEVELOPMENT
 const config = {
   development: {
-    username: 'postgres',
-    password: 'Tomzriderx02',
-    database: 'db-cursed-two',
-    host: 'localhost',
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
     dialect: 'postgres',
     dialecModule: pg,
   },
   test: {
-    username: 'postgres',
-    password: 'Tomzriderx02',
-    database: 'db-cursed-two',
-    host: 'localhost',
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
     dialect: 'postgres',
     dialecModule: pg,
   },
   production: {
-    username: 'postgres',
-    password: 'Tomzriderx02',
-    database: 'db-cursed-two',
-    host: 'localhost',
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
     dialect: 'postgres',
     dialecModule: pg,
   },
 }
+
+// DEVELOPMENT
+// const config = {
+//   development: {
+//     username: 'postgres',
+//     password: 'Tomzriderx02',
+//     database: 'db-cursed-two',
+//     host: 'localhost',
+//     dialect: 'postgres',
+//     dialecModule: pg,
+//   },
+//   test: {
+//     username: 'postgres',
+//     password: 'Tomzriderx02',
+//     database: 'db-cursed-two',
+//     host: 'localhost',
+//     dialect: 'postgres',
+//     dialecModule: pg,
+//   },
+//   production: {
+//     username: 'postgres',
+//     password: 'Tomzriderx02',
+//     database: 'db-cursed-two',
+//     host: 'localhost',
+//     dialect: 'postgres',
+//     dialecModule: pg,
+//   },
+// }
 
 module.exports = config
